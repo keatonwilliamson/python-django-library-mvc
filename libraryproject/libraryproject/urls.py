dfbdeb90d1b4d4ed17283f3b16690a2510ab3178
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-17 19:52:35 -0600

    first migration and super user created

--- libraryproject/libraryproject/urls.py
+++ libraryproject/libraryproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from libraryapp.models import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
