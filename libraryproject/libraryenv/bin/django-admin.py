7e541d3e47d4eef2c49cc622dee39b2150ac273c
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-17 18:48:34 -0600

    first commit

---
+++ libraryproject/libraryenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/keatonwilliamson/workspace/D36-Demos/library-management/libraryproject/libraryenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
