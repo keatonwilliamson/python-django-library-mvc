62838e2188ca35d8fd8c3853563680d68cad4c78
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-17 19:36:56 -0600

    added librarian and updated book class

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .library import Library
+from .librarian import Librarian
 
 class Book(models.Model):
 
@@ -6,6 +8,8 @@ class Book(models.Model):
     ISBN = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year_published = models.IntegerField()
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Book")
