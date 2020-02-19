5fd5905b54d381159bef9354ca551ae3e6c7ac6c
Keaton Williamson <georgekwilliamson@gmail.com>
2020-02-19 14:52:45 -0600

    books added to library list view. chapter 10 done

--- libraryproject/libraryapp/views/books/details.py
+++ libraryproject/libraryapp/views/books/details.py
@@ -2,26 +2,58 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from libraryapp.models import Book, Library
+from libraryapp.models import Book, Library, Librarian
 from libraryapp.models import model_factory
 from ..connection import Connection
 
 
+def create_book(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.author = _row["author"]
+    book.isbn = _row["isbn"]
+    book.title = _row["title"]
+    book.year_published = _row["year_published"]
+
+    librarian = Librarian()
+    librarian.id = _row["librarian_id"]
+    librarian.first_name = _row["first_name"]
+    librarian.last_name = _row["last_name"]
+
+    library = Library()
+    library.id = _row["library_id"]
+    library.title = _row["library_name"]
+
+    book.librarian = librarian
+    book.location = library
+
+    return book
+
 def get_book(book_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Book)
+        conn.row_factory = create_book
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
-            b.id,
+            b.id book_id,
             b.title,
             b.isbn,
             b.author,
             b.year_published,
             b.librarian_id,
-            b.location_id
+            b.location_id,
+            li.id librarian_id,
+            u.first_name,
+            u.last_name,
+            loc.id library_id,
+            loc.title library_name
         FROM libraryapp_book b
+        JOIN libraryapp_librarian li ON b.librarian_id = li.id
+        JOIN libraryapp_library loc ON b.location_id = loc.id
+        JOIN auth_user u ON u.id = li.user_id
         WHERE b.id = ?
         """, (book_id,))
 
--- libraryproject/libraryapp/views/libraries/list.py
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -1,39 +1,78 @@
 import sqlite3
 from django.shortcuts import render
-from libraryapp.models import Library
+from libraryapp.models import Library, Book
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.shortcuts import redirect
 
+
+def create_library(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    library = Library()
+    library.id = _row["id"]
+    library.title = _row["title"]
+    library.address = _row["address"]
+
+    # Note: You are adding a blank books list to the library object
+    # This list will be populated later (see below)
+    library.books = []
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.title = _row["book_title"]
+    book.author = _row["author"]
+    book.isbn = _row["isbn"]
+    book.year_published = _row["year_published"]
+
+    # Return a tuple containing the library and the
+    # book built from the data in the current row of
+    # the data set
+    return (library, book,)
+
 @login_required
 def library_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
+            conn.row_factory = create_library
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
-            select
-                l.id,
-                l.title,
-                l.address
-            from libraryapp_library l
+                SELECT
+                    li.id,
+                    li.title,
+                    li.address,
+                    b.id book_id,
+                    b.title book_title,
+                    b.author,
+                    b.year_published,
+                    b.isbn
+                FROM libraryapp_library li
+                JOIN libraryapp_book b ON li.id = b.location_id
             """)
 
-            all_libraries = []
-            dataset = db_cursor.fetchall()
+            libraries = db_cursor.fetchall()
+            library_groups = {}
 
-            for row in dataset:
-                library = Library()
-                library.id = row['id']
-                library.title = row['title']
-                library.address = row['address']
-                all_libraries.append(library)
+            # Iterate the list of tuples
+            for (library, book) in libraries:
+
+                # If the dictionary does have a key of the current
+                # library's `id` value, add the key and set the value
+                # to the current library
+                if library.id not in library_groups:
+                    library_groups[library.id] = library
+                    library_groups[library.id].books.append(book)
+
+                # If the key does exist, just append the current
+                # book to the list of books for the current library
+                else:
+                    library_groups[library.id].books.append(book)
 
         template = 'libraries/list.html'
         context = {
-            'all_libraries': all_libraries
+            'all_libraries': library_groups.values()
         }
 
         return render(request, template, context)
